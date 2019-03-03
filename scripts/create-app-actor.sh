6660094be63ee659597501d5a36003600ac5243b
Ivan Malone <ivanincanada@gmail.com>
2019-03-03 02:32:14 -0800

    added json to build scripts

--- scripts/create-app-actor.sh
+++ scripts/create-app-actor.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 cp src/templates/App.js.template "./src/${1}.js"
+cp src/templates/App.json.template "./src/${1}.json"
+
 sed -i "s/{App}/${1}/g" "./src/${1}.js"
+sed -i "s/{App}/${1}/g" "./src/${1}.json"
+
 echo "Created: " + "./src/${1}.js"
+echo "Created: " + "./src/${1}.json"
--- scripts/create-child-actor.sh
+++ scripts/create-child-actor.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 cp ./src/templates/AppChild.js.template "./src/actors/${1}.js"
+cp ./src/templates/AppChild.json.template "./src/actors/${1}.json"
 sed -i "s/{AppChild}/${1}/g" "/src/actors/${1}.js"
+sed -i "s/{AppChild}/${1}/g" "/src/actors/${1}.json"
 echo "Created: " + "./src/actors/${1}.js"
+echo "Created: " + "./src/actors/${1}.json"
