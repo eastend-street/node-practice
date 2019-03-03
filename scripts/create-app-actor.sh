dc96b1ac3e69ce752c03a2e15ec64768f5b22569
Ivan Malone <ivanincanada@gmail.com>
2019-03-03 02:58:26 -0800

    updated scripts

--- scripts/create-app-actor.sh
+++ scripts/create-app-actor.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 cp src/templates/App.js.template "./src/${1}.js"
 cp src/templates/App.json.template "./src/${1}.json"
+cp src/templates/App.logger.template "./src/${1}.logger"
 
 sed -i "s/{App}/${1}/g" "./src/${1}.js"
 sed -i "s/{App}/${1}/g" "./src/${1}.json"
+sed -i "s/{App}/${1}/g" "./src/${1}.logger"
 
 echo "Created: " + "./src/${1}.js"
 echo "Created: " + "./src/${1}.json"
+echo "Created: " + "./src/${1}.jogger"
--- scripts/create-child-actor.sh
+++ scripts/create-child-actor.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 cp ./src/templates/AppChild.js.template "./src/actors/${1}.js"
 cp ./src/templates/AppChild.json.template "./src/actors/${1}.json"
+cp ./src/templates/AppChild.json.template "./src/actors/${1}.logger"
+
 sed -i "s/{AppChild}/${1}/g" "/src/actors/${1}.js"
 sed -i "s/{AppChild}/${1}/g" "/src/actors/${1}.json"
+sed -i "s/{AppChild}/${1}/g" "/src/actors/${1}.logger"
+
 echo "Created: " + "./src/actors/${1}.js"
 echo "Created: " + "./src/actors/${1}.json"
+echo "Created: " + "./src/actors/${1}.logger"
