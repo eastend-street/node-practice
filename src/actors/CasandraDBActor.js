d13c8419fa4a38c04bb6339b8a1c4862eb31644d
Ivan Malone <ivanincanada@gmail.com>
2019-03-17 03:13:09 -0700

    npm install casandra-driver

---
+++ src/actors/CasandraDBActor.js
@@ -0,0 +1,32 @@
+
+
+class CasandraDBActor
+	{
+	initialize(selfActor)
+		{
+		this.self = selfActor;
+		this.mongodb = require('mongodb').MongoClient;
+		this.url = "mongodb://database:27017/" + this.name;
+
+		this.mongodb.connect(this.url, function(err,db)
+			{
+			if (err) throw err;
+			console.log("Database Actor created!");
+			db.close();
+			});
+		}
+
+	update(data)
+		{
+		this.mongodb.connect(this.url, function(err,db)
+                        {
+                        if (err) throw err;
+                        console.log("Database Actor Updated!");
+                        db.close();
+                        });
+
+		}
+
+	}
+
+module.exports = CasandraDBActor
